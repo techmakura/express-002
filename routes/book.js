7c9d51225c5ebf1c632646af938fefb5dd02622b
binaya346 <rijalbinaya@gamil.com>
2025-08-24 16:26:50 +0545

    completed crud in books

--- routes/book.js
+++ routes/book.js
@@ -6,40 +6,82 @@ const BookModel = require("../model/book");
 router.get('/', async (req, res) => {
     try {
         const books = await BookModel.find({});
-
         console.log(books + " books");
         res.send({ "result": books });
     } catch (err) {
-        res.send({"Error": "This is some error"});
+        res.send({ "Error": "This is some error" });
     }
 });
 
 // Get book that matches provided id
-router.get('/:id', (req, res) => {
-
-    res.send({ "result": "THis will return the book that matches the id" });
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const books = await BookModel.findById(id).exec();
+        console.log(books + " books");
+        res.send({ "result": books });
+    } catch (err) {
+        res.send({ "Error": "This is some error" });
+    }
 });
-
 // (Create) a new book
-router.post("/", (req, res) => {
-    const { username, email, password } = req.body;
-    console.log("Username: " + username + " password: " + password + " email: " + email)
+router.post("/", async (req, res) => {
+    try {
+        const { title, pages, price, language, published_year } = req.body;
 
-    res.send({ "message": "Successfully created!" })
+        const newBook = {
+            "title": title,
+            "pages": pages,
+            "price": price,
+            "language": language,
+            "published_year": published_year
+        }
+        const Books = new BookModel(newBook);
+        const response = await Books.save()
+        res.send({ "message": `Successfully created! ${response}` })
+    } catch (err) {
+        console.log(err + " err");
+        res.send({ "Error": "This is some error" });
+    }
 });
 
 // (updated) Update the book with provided id
-router.put("/:id", (req, res) => {
-    const id = req.params.id;
-    console.log(id + " id")
-    const q = req.query.q;
-    const token = req.headers.token;
-    res.send({ "message": "Successfully updated! id: " + token })
+router.put("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const { title, pages, price, language, published_year } = req.body;
+
+        const newBook = {
+            "title": title,
+            "pages": pages,
+            "price": price,
+            "language": language,
+            "published_year": published_year
+        }
+
+        const response = await BookModel.findByIdAndUpdate(
+            id,
+            { $set: newBook },
+            { new: true, runValidators: true }
+        );
+
+        res.send({ "message": "Successfully updated!" + response })
+    } catch (err) {
+        res.send({ "Error": "This is some error" });
+    }
 });
 
 // (Deleted) the book that matches the provided id
-router.delete("/:id", (req, res) => {
-    res.send({ "message": "Successfully Deleted!" })
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const response = await BookModel.findByIdAndDelete(id);
+
+        res.send({ "message": `Successfully Deleted! ${response}` })
+    } catch (err) {
+        res.send({ "Error": "This is some error" });
+    }
 });
 
 module.exports = router;
